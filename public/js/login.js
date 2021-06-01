e8f12a190e0d83ff4903e077516e4658e308ce8a
Allyson Reis <allysonreis1@gmail.com>
2021-06-01 11:01:30 -0300

    Alterando animacao login com modelo do codepen

--- public/js/login.js
+++ public/js/login.js
@@ -1,42 +1,11 @@
-const login = document.querySelector('.login-container');
-const createAccount = document.querySelector('.create-account-container');
-const overlay = document.querySelector('.overlay');
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-let flag = true;
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
 
-overlay.addEventListener('click', e => {
-
-  if (flag) {
-    overlay.classList.add('overlay-animToRight');
-    overlay.classList.remove('overlay-animToLeft');
-    
-    login.classList.add('formToLeft-in');
-    login.classList.remove('formToLeft-out');
-    login.style.display = 'flex';
-
-    createAccount.classList.add('formToRight-out');
-    createAccount.classList.remove('formToRight-in');
-
-    setTimeout(()=> {
-      createAccount.style.display = 'none';
-    }, 900);
-
-    flag = false;
-  } else {
-    overlay.classList.add('overlay-animToLeft');  
-    overlay.classList.remove('overlay-animToRight');
-    
-    login.classList.remove('formToLeft-in');
-    login.classList.add('formToLeft-out');
-
-    createAccount.classList.add('formToRight-in');
-    createAccount.classList.remove('formToRight-out');
-    createAccount.style.display = 'flex';
-
-    setTimeout(()=> {
-      login.style.display = 'none';
-    }, 900);
-
-    flag = true;
-  }
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
 });
